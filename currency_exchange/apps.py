3b92730001f04eb278926e64ee01754e8186a3ea
Abhishek Pandey <Abhishek.Pandey@helloverify.com>
2020-10-17 14:10:14 +0530

    project

--- currency_exchange/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CurrentExchangeConfig(AppConfig):
-    name = 'current_exchange'
--- currency_exchange/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class currency_details(models.Model):
-    currency_name = models.CharField(max_length=50, default='')
-    currency_abbrev = models.CharField(max_length=5, default='')
